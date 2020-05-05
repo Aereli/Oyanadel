fd522b5f188ef152cf23235ecff9363b3a2d2a2c
Eric <Aerelioyanadel@gmail.com>
2020-05-05 12:48:19 -0400

    fix project styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Single from "./single"
 import { useStaticQuery, graphql } from "gatsby"
+import "../styles/projects.css"
 
 const Projects = () => {
   const Info = useStaticQuery(graphql`
@@ -29,32 +30,28 @@ const Projects = () => {
   const projects = Info.allProjectsJson.edges
 
   return (
-    <div
-      style={{
-        backgroundImage: `linear-gradient(to  right, #e0d6cc 50%, #192231 50%)`,
-      }}
-    >
-      {projects.map(({ node: data }, id) => {
-        const title = data.title
-        const sub = data.sub
-        const description = data.description
-        const image = data.image.childImageSharp.fluid
-        const github = data.github
-        const url = data.url
-        return (
-          <Single
-            key={id}
-            title={title}
-            sub={sub}
-            description={description}
-            image={image}
-            github={github}
-            url={url}
-          />
-        )
-      })}
-
-      <div></div>
+    <div className="project-container">
+      <div className="project-margin">
+        {projects.map(({ node: data }, id) => {
+          const title = data.title
+          const sub = data.sub
+          const description = data.description
+          const image = data.image.childImageSharp.fluid
+          const github = data.github
+          const url = data.url
+          return (
+            <Single
+              key={id}
+              title={title}
+              sub={sub}
+              description={description}
+              image={image}
+              github={github}
+              url={url}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/components/single.js
+++ src/components/single.js
@@ -3,17 +3,17 @@ import Img from "gatsby-image"
 import "../styles/projects.css"
 // import { Link } from "gatsby"
 
-const Single = ({ image, title, description, sub, id, github, url }) => {
+const Single = ({ image, title, url, description, sub, id, github }) => {
   return (
     <>
-      <div className="project-container">
+      <div className="indivdual-project-container">
         <div className="image">
           <div className="inner-image">
             <p className="inner-text">{title}</p>
             <p>{sub}</p>
             <p>{description}</p>
-            <a>{url}</a>
-            <a>{github}</a>
+            <a>url {url}</a>
+            <a>github {github}</a>
           </div>
           <Img key={id} fluid={image}></Img>
         </div>
