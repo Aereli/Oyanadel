a6b15dad3acfa124fb064cbc015d08adb4d8c021
Eric <Aerelioyanadel@gmail.com>
2020-05-06 11:49:51 -0400

    fix config for images path

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/data`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-json`,
