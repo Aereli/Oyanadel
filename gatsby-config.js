24585d29a032c1d8c29c3f80aaf255b340cc416e
Eric <Aerelioyanadel@gmail.com>
2020-05-06 13:58:54 -0400

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo_2.svg`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.svg`,
+            sizes: `192x192`,
+            type: `image/svg`,
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
