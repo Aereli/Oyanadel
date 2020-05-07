84da1b7f6238dc004c199e64de06db6b9e2a32a6
Eric <Aerelioyanadel@gmail.com>
2020-05-07 16:44:41 -0400

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Portfolio site of Eric Oyanadel`,
     author: `@Aereli`,
   },
+
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -26,24 +27,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Oyanadel`,
+        short_name: `Aereli`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo_2.svg`, // This path is relative to the root of the site.
-        icons: [
-          {
-            src: `/favicons/android-chrome-192x192.svg`,
-            sizes: `192x192`,
-            type: `image/svg`,
-          },
-        ],
+        icon: `src/images/green-window-SINGLE.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
